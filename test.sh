01fb4c6842f0576d673fdecd5bd03e5c263baf2c
Dirk Thomas <dthomas@osrfoundation.org>
2014-01-15 07:27:51 -0800

    make it really minimal, create simple file with boost include, ditch all qt_gui related stuff

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
- #!/bin/sh
+#!/bin/sh
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu raring main" > /etc/apt/sources.list.d/ros-latest.list'
+#gdb --args
+/usr/bin/shiboken --generatorSet=shiboken --include-paths=./include::/usr/include/qt4 --typesystem-paths=/usr/share/PySide/typesystems --output-directory=./out global.h typesystem.xml
 
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-
-sudo apt-get update
-
-sudo apt-get install shiboken
-sudo apt-get install ros-hydro-qt-gui-core
-
-gdb --args /usr/bin/shiboken --generatorSet=shiboken --include-paths=./include-paths::/usr/include/qt4 --typesystem-paths=/usr/share/PySide/typesystems --output-directory=./out global.h typesystem.xml
